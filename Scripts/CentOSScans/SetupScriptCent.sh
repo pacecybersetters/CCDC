c1a066f7602423ed1b199af276c655064aaad4e9
dbarr914 <dbarr914@gmail.com>
2020-01-06 14:12:15 -0500

    Update SetupScriptCent.sh

--- Scripts/CentOSScans/SetupScriptCent.sh
+++ Scripts/CentOSScans/SetupScriptCent.sh
@@ -21,23 +21,40 @@
 # ---------------------------------------------------------------------
 
 read -p "\e[93m What is the IP Address of the Splunk Indexer? \e[0m" indexerip
-
+echo
+read -p "What is the user's home directory where the git repo is stored? " userhome
+echo
 #                         INITIAL UPDATE
 # ---------------------------------------------------------------------
-
-sudo yum clean all
-sudo yum -y update
-
-#                       YUM PACKAGES INSTALL
+#
+# Install GITHUB, WGET, LSB_RELEASE, NMAP
+echo -e "\e[92mDate Run: $(date)"
+echo
+echo -e "This script will install OSQUERY 4.1.1 endpoint visibility agent,"
+echo -e "and Splunk Universal Forwarder and other dependencies. In addition" 
+echo -e "it will download the predetermined configuration files.\e[0m "
+echo
+echo -e "\e[95mUpdating System..."
+echo -e "This may take some time..."
+sudo yum clean all > /dev/null 2>&1
+echo "..................."
+sudo yum -y update > /dev/null 2>&1
+echo "[*] Complete."
+echo
+#                         YUM PACKAGES INSTALL
 # ---------------------------------------------------------------------
-
-sudo yum -y install git wget redhat-lsb-core nmap yum-utils lsof epel-release
-
+echo "Installing Dependencies..."
+echo
+sudo yum -y install git wget redhat-lsb-core nmap yum-utils lsof epel-release > /dev/null 2>&1
+echo
+echo -e "[*] Complete.\e[0m"
+echo
 #                         CONFIG DOWNLOADS
 # ---------------------------------------------------------------------
 
 # cd ~/Documents
 # git clone https://github.com/dbarr914/CCDC.git
+
 
 #                           SPLUNK FORWARDER INSTALL
 # ---------------------------------------------------------------------
@@ -45,18 +62,18 @@ sudo yum -y install git wget redhat-lsb-core nmap yum-utils lsof epel-release
 download_splunk(){
  cd /tmp
  echo
- echo "\e[93m[*] Downloading Splunk Universal Forwarder.....\e[0m"
+ echo -e "\e[93m[*] Downloading Splunk Universal Forwarder.....\e[0m"
  wget -O splunkforwarder-8.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.1&product=universalforwarder&filename=splunkforwarder-8.0.1-6db836e2fb9e-Linux-x86_64.tgz&wget=true'
  echo
- echo "\e[93m[*] Splunk UFW Downloaded.\e[0m"
+ echo -e "\e[93m[*] Splunk UFW Downloaded.\e[0m"
  echo
  }
 
 install_splunk(){
- echo "\e[93m[*] Installing Splunk Universal Forwarder.....\e[0m"
+ echo -e "\e[93m[*] Installing Splunk Universal Forwarder.....\e[0m"
  sudo tar -xzvf /tmp/splunk-8.tgz -C /opt
  echo
- echo "\e[93m[*] Splunk UFW Installed.\e[0m"
+ echo -e "\e[93m[*] Splunk UFW Installed.\e[0m"
  rm -f /tmp/splunk-8.tgz
 }
 
@@ -69,15 +86,60 @@ add_user(){
  echo
 } 
 
-download_splunk
-install_splunk
-add_user
+initial_run(){
+ echo
+ echo "[*] Running initial start....."
+ echo
+ /opt/splunk/bin/splunk start --accept-license
+ /opt/splunk/bin/splunk stop /dev/null 2>&1
+ echo
+ echo "[*] Complete."
+ echo
+ echo "[*] Enabling Splunk to start at boot....."
+ echo
+ /opt/splunk/bin/splunk enable boot-start
+ echo
+ echo "[*] Complete."
+ echo
+}
 
-cd /opt/splunkforwarder/bin
-sudo ./splunk start --accept-license
-sudo ./splunk enable boot-start
-sudo ./splunk add forward-server "$indexerip":9997
-sudo ./splunk restart
+#                           EDIT SPLUNK INPUTS 
+# ---------------------------------------------------------------------
+
+edit_inputs(){
+ echo "[*] Editing Splunk's input file...."
+
+ cd /opt/splunk/etc/system/local
+
+ echo -e "[monitor:///var/log/osquery/osqueryd.results.log]\nindex = osquery\nsourcetype = osquery:results\n\n" >> inputs.conf
+ echo -e "[monitor:///var/log/osquery/osqueryd.*ERROR*]\nindex = osquery\nsourcetype = osquery:error\n\n" >> inputs.conf
+ echo -e "[monitor:///var/log/osquery/osqueryd.*WARNING*]\nindex = osquery\nsourcetype = osquery:warning\n\n" >> inputs.conf
+ echo -e "[monitor:///var/log/osquery/osqueryd.snapshot.log\nindex = osquery\nsourcetype = osquery:snapshots\n\n" >> inputs.conf
+ 
+ echo "[*] Complete."
+ echo "[*] Adding directories to monitor..." 
+ echo
+ cd /opt/splunk/bin/
+
+ # sudo ./splunk add monitor /var/log
+ # sudo ./splunk add monitor /etc/
+ 
+ echo "[*] Complete."
+ echo
+ echo "[*] Adding forward-server..." 
+ echo
+ sudo ./splunk add forward-server "$indexerip":9997
+ echo
+ echo "[*] Complete."
+ echo
+ echo "[*] Restarting Splunk..."
+ echo
+ sudo ./splunk restart > /dev/null 2>&1
+ echo
+ sudo ./splunk status
+ echo "[*] Complete."
+ echo
+}
 
 #                          OSQUERY INSTALL
 # ---------------------------------------------------------------------
@@ -100,46 +162,25 @@ install_osquery(){
  rm -f /tmp/osquery-4.1.1-1.linux.x86_64.rpm
 }
 
-download_osquery
-install_osquery
-
 #                    MOVE CONFIGS TO CORRECT LOCATIONS
 # ---------------------------------------------------------------------
+
 config_osquery(){
- sudo cp ./Documents/CCDC-master/osquery/1.Linux/osquery.conf /etc/osquery/osquery.conf
- sudo cp ./Documents/CCDC-master/osquery/1.Linux/osquery.flags /etc/osquery/osquery.flags
- sudo cp -rf ./Documents/CCDC-master/osquery/1.Linux/packs/ /etc/osquery/packs
- sudo cp -rf ./Documents/CCDC-master/osquery/1.Linux/packs/ /usr/share/osquery/packs
- sudo osqueryctl config-check
- sudo osqueryctl start
+
+ cp "/home/$userhome/Documents/CCDC/osquery/1.Linux/osquery.conf" /etc/osquery/osquery.conf
+ cp "/home/$userhome/Documents/CCDC/osquery/1.Linux/osquery.flags" /etc/osquery/osquery.flags
+ cp -rf "/home/$userhome/Documents/CCDC/osquery/1.Linux/packs/" /etc/osquery/
+ cp -rf "/home/$userhome/Documents/CCDC/osquery/1.Linux/packs/" /usr/share/osquery/
+
+ osqueryctl config-check
+ osqueryctl start
 }
 
+download_splunk
+install_splunk
+add_user
+initial_run
+download_osquery
+install_osquery
 config_osquery
-
-
-#                         CONFIGURING INPUTS.CONF
-# ---------------------------------------------------------------------
-
-edit_inputs(){
- 
- echo "[*] Editing Splunk's input file...."
- cd /opt/splunkforwarder/etc/system/local 
- echo -e "[monitor:///var/log/osquery/osqueryd.results.log]\nindex = osquery\nsourcetype = osquery:results\n\n" >> inputs.conf
- echo -e "[monitor:///var/log/osquery/osqueryd.*ERROR*]\nindex = osquery\nsourcetype = osquery:error\n\n" >> inputs.conf
- echo -e "[monitor:///var/log/osquery/osqueryd.*WARNING*]\nindex = osquery\nsourcetype = osquery:warning\n\n" >> inputs.conf
- echo -e "[monitor:///var/log/osquery/osqueryd.snapshot.log\nindex = osquery\nsourcetype = osquery:snapshots\n\n" >> inputs.conf
- echo "[*] Complete."
- 
- cd /opt/splunkforwarder/bin/
- 
- echo "[*] Complete."
- echo
- echo "[*] Restarting Splunk..."
- 
- sudo ./splunk restart
- 
- echo "[*] Complete."
- echo
-}
-
 edit_inputs
